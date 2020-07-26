4be3ab32c0f69ba33309603ff03d468ada7aea68
Ethan Shook <eshook89@gmail.com>
2020-07-26 17:17:02 -0500

    more

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 const startButton = document.getElementById("start-btn");
-const questionContainerElement - document.getElementById("question-container")
+const nextButton = document.getElementById("next-btn");
 
-const shuffledQuestions, currentQuestionIndex
+const questionContainerElement - document.getElementById("question-container")
+const questionElement = document.getElementById("question")
+const answerButtonsElement = document.getElementById("answer-buttons")
+
+let shuffledQuestions, currentQuestionIndex
 
 startButton.addEventListener("click", startGame);
+nextButton.addEventListener("click", () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
 
 function startGame() {
     console.log("Started")
@@ -14,9 +22,61 @@ function startGame() {
     setNextQuestion()
 }
 
-function setNextQuestion() {}
+function setNextQuestion() {
+    resetState()
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
 
-function selectAnswer() {}
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.foreach(answer => {
+        const button = document.createElement("button")
+        button.innerText = answer.textbutton.classList.add("btn")
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener("click", selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+    }
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add("hide")
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild
+        (answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.datset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove("hide")
+    } else {
+        startButton.innerText = "Restart"
+        startButton.classList.remove("hide")
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add("correct")
+    } else {
+        element.classList.add("wrong")
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove("correct")
+    element.classList.remove("wrong")
+}
 
 const questions = [
     {
